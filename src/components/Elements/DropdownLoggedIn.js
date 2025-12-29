777c1e55f2a0e31b9ea045d23388c195fe395ebe
Nikunj Jain <nikunjjain@Nikunjs-MacBook-Air.local>
2025-12-29 09:34:32 +0530

    Added guest login

--- src/components/Elements/DropdownLoggedIn.js
+++ src/components/Elements/DropdownLoggedIn.js
@@ -22,7 +22,6 @@ export const DropdownLoggedIn = ({ setDropDown }) => {
     };
     fetchUser();
   }, [ebid, token]);
-  console.log(user);
   const navigate = useNavigate();
   const handleLogout = () => {
     logout();
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -6,6 +6,28 @@ import { useTitle } from "../hooks/useTitle";
 export const Login = () => {
   useTitle("Login");
   const navigate = useNavigate();
+
+  const handleLoginGuest = async () => {
+    try {
+      const authDetail = {
+        email: process.env.REACT_APP_EMAIL,
+        password: process.env.REACT_APP_PASSWORD,
+      };
+
+      const data = await login(authDetail);
+
+      if (data.accessToken) {
+        sessionStorage.setItem("token", JSON.stringify(data.accessToken));
+        sessionStorage.setItem("ebid", JSON.stringify(data.user.id));
+        navigate("/products");
+      } else {
+        toast.error("User not found");
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
   const handleLogin = async (event) => {
     event.preventDefault();
     try {
@@ -71,10 +93,10 @@ export const Login = () => {
         </button>
       </form>
       <button
-        type="submit"
-        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        onClick={handleLoginGuest}
+        className="mt-3 cursor-pointer text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-3 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
       >
-        Log In as Guest
+        Login As Guest
       </button>
     </main>
   );
